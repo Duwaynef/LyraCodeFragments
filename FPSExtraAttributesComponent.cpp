8cb49a83d3a6f858388f5a20fc386fb05627a136
Duwayne <Silent1234@gmail.com>
2023-01-14 10:36:57 -0700

    Update FPSExtraAttributesComponent.cpp

--- FPSExtraAttributesComponent.cpp
+++ FPSExtraAttributesComponent.cpp
@@ -1,13 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/FPSExtraAttributesComponent.h"
+#include "FPSExtraAttributesComponent.h"
 
 #include "LyraLogChannels.h"
+#include "AbilitySystem/LyraAbilitySystemComponent.h"
+#include "FPSExtraAttributes.h"
 #include "GameplayEffect.h"
 #include "GameplayEffectExtension.h"
-#include "AbilitySystem/LyraAbilitySystemComponent.h"
-#include "Character/FPSExtraAttributes.h"
+#include "Messages/LyraVerbMessageHelpers.h"
 
 UFPSExtraAttributesComponent::UFPSExtraAttributesComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -34,18 +35,20 @@ void UFPSExtraAttributesComponent::InitializeWithAbilitySystem(ULyraAbilitySyste
 	AbilitySystemComponent = InASC;
 	if (!AbilitySystemComponent)
 	{
-		UE_LOG(LogLyra, Error, TEXT("FPSExtraAttributesComponent: Cannot initialize component for owner [%s] with NULL ability system."), *GetNameSafe(Owner));
+		UE_LOG(LogLyra, Error, TEXT("FPSExtraAttributesComponent: Cannot initialize AbilitySystemComponent component for owner [%s] with NULL ability system."), *GetNameSafe(Owner));
 		return;
 	}
 
 	FPSExtraAttributes = AbilitySystemComponent->GetSet<UFPSExtraAttributes>();
 	if (!FPSExtraAttributes)
 	{
-		UE_LOG(LogLyra, Error, TEXT("FPSExtraAttributesComponent: Cannot initialize component for owner [%s] with NULL set on the ability system."), *GetNameSafe(Owner));
+		UE_LOG(LogLyra, Error, TEXT("FPSExtraAttributesComponent: Cannot initialize FPSExtraAttributes component for owner [%s] with NULL set on the ability system."), *GetNameSafe(Owner));
 		return;
 	}
 
 	// Register to listen for attribute changes.
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSExtraAttributes::GetArmorAttribute()).AddUObject(this, &ThisClass::HandleArmorChanged);
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSExtraAttributes::GetMaxArmorAttribute()).AddUObject(this, &ThisClass::HandleMaxArmorChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSExtraAttributes::GetHPRegenAttribute()).AddUObject(this, &ThisClass::HandleHPRegenChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSExtraAttributes::GetArmorRegenAttribute()).AddUObject(this, &ThisClass::HandleArmorRegenChanged);	
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSExtraAttributes::GetStaminaRegenAttribute()).AddUObject(this, &ThisClass::HandleStaminaRegenChanged);
@@ -56,6 +59,29 @@ void UFPSExtraAttributesComponent::UninitializeFromAbilitySystem()
 {
 	FPSExtraAttributes = nullptr;
 	AbilitySystemComponent = nullptr;
+}
+
+float UFPSExtraAttributesComponent::GetArmor() const
+{
+	return (FPSExtraAttributes ? FPSExtraAttributes->GetArmor() : 0.0f);
+}
+
+float UFPSExtraAttributesComponent::GetMaxArmor() const
+{
+	return (FPSExtraAttributes ? FPSExtraAttributes->GetMaxArmor() : 0.0f);
+}
+
+float UFPSExtraAttributesComponent::GetArmorNormalized() const
+{
+	if (FPSExtraAttributes)
+	{
+		const float Armor = FPSExtraAttributes->GetArmor();
+		const float MaxArmor = FPSExtraAttributes->GetMaxArmor();
+
+		return ((MaxArmor > 0.0f) ? (Armor / MaxArmor) : 0.0f);
+	}
+
+	return 0.0f;
 }
 
 float UFPSExtraAttributesComponent::GetHPRegen() const
@@ -73,13 +99,18 @@ float UFPSExtraAttributesComponent::GetStaminaRegen() const
 	return (FPSExtraAttributes ? FPSExtraAttributes->GetStaminaRegen() : 0.0f);
 }
 
+float UFPSExtraAttributesComponent::GetManaRegen() const
+{
+	return (FPSExtraAttributes ? FPSExtraAttributes->GetManaRegen() : 0.0f);
+}
+
 void UFPSExtraAttributesComponent::OnUnregister()
 {
 	UninitializeFromAbilitySystem();
 	Super::OnUnregister();
 }
 
-static AActor* GetInstigatorFromAttrChangeDataHPRegen(const FOnAttributeChangeData& ChangeData)
+static AActor* GetInstigatorFromAttrChangeDataFPSExtraAttributes(const FOnAttributeChangeData& ChangeData)
 {
 	if (ChangeData.GEModData != nullptr)
 	{
@@ -90,39 +121,32 @@ static AActor* GetInstigatorFromAttrChangeDataHPRegen(const FOnAttributeChangeDa
 	return nullptr;
 }
 
-static AActor* GetInstigatorFromAttrChangeDataArmorRegen(const FOnAttributeChangeData& ChangeData)
+void UFPSExtraAttributesComponent::HandleArmorChanged(const FOnAttributeChangeData& ChangeData)
 {
-	if (ChangeData.GEModData != nullptr)
-	{
-		const FGameplayEffectContextHandle& EffectContext = ChangeData.GEModData->EffectSpec.GetEffectContext();
-		return EffectContext.GetOriginalInstigator();
-	}
-
-	return nullptr;
+	OnArmorChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
 }
 
-static AActor* GetInstigatorFromAttrChangeDataStaminaRegen(const FOnAttributeChangeData& ChangeData)
+void UFPSExtraAttributesComponent::HandleMaxArmorChanged(const FOnAttributeChangeData& ChangeData)
 {
-	if (ChangeData.GEModData != nullptr)
-	{
-		const FGameplayEffectContextHandle& EffectContext = ChangeData.GEModData->EffectSpec.GetEffectContext();
-		return EffectContext.GetOriginalInstigator();
-	}
-
-	return nullptr;
+	OnMaxArmorChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
 }
 
 void UFPSExtraAttributesComponent::HandleHPRegenChanged(const FOnAttributeChangeData& ChangeData)
 {
-	OnHPRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataHPRegen(ChangeData));
+	OnHPRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
 }
 
 void UFPSExtraAttributesComponent::HandleArmorRegenChanged(const FOnAttributeChangeData& ChangeData)
 {
-	OnArmorRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataArmorRegen(ChangeData));
+	OnArmorRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
 }
 
 void UFPSExtraAttributesComponent::HandleStaminaRegenChanged(const FOnAttributeChangeData& ChangeData)
 {
-	OnStaminaRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataStaminaRegen(ChangeData));
+	OnStaminaRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
+}
+
+void UFPSExtraAttributesComponent::HandleManaRegenChanged(const FOnAttributeChangeData& ChangeData)
+{
+	OnManaRegenChanged.Broadcast(this, ChangeData.OldValue, ChangeData.NewValue, GetInstigatorFromAttrChangeDataFPSExtraAttributes(ChangeData));
 }
