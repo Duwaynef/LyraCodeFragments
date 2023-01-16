99b9bd598b49d9931333722666dbbf5aba0226a5
Duwayne <Silent1234@gmail.com>
2023-01-16 15:40:03 -0700

    Update MyBlueprintFunctionLibrary.cpp

--- MyBlueprintFunctionLibrary.cpp
+++ MyBlueprintFunctionLibrary.cpp
@@ -1,9 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BlueprintLibrary/MyBlueprintFunctionLibrary.h"
+
+#include "FPSRogueLike/BlueprintLibrary/MyBlueprintFunctionLibrary.h"
+#include "GameModes/LyraGameMode.h"
+#include "Components/GameStateComponent.h"
+#include "Components/ActorComponent.h"
+#include "FPSRogueLike/Gameplay/FPSGameMode.h"
+#include "Engine/World.h"
+#include "GameModes/LyraExperienceDefinition.h"
+#include "Character/LyraPawnExtensionComponent.h"
+#include "GameModes/LyraExperienceManagerComponent.h"
+#include "GameFramework/PlayerState.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+#include "AIController.h"
+#include "Character/LyraPawnExtensionComponent.h"
 
 void UMyBlueprintFunctionLibrary::GiveActorAbilityWithInputTag(AActor* TargetActor,
-	UAbilitySystemComponent* AbilitySystemComponent, FMyBlueprintFunctionLibrary_GameplayAbilityStruct AbilityStruct)
+                                                               UAbilitySystemComponent* AbilitySystemComponent, FMyBlueprintFunctionLibrary_GameplayAbilityStruct AbilityStruct)
 {
 	check(AbilitySystemComponent);
 	
@@ -45,4 +59,36 @@ void UMyBlueprintFunctionLibrary::GiveActorAbilityWithInputTag(AActor* TargetAct
 
 	AbilitySystemComponent->GiveAbility(AbilitySpec);
 	
+}
+
+void UMyBlueprintFunctionLibrary::SpawnEnemy(UGameStateComponent* GameStateComponent,  AFPSGameMode* GameMode, TSubclassOf<AAIController> BotControllerClass)
+{
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+	SpawnInfo.OverrideLevel = GameStateComponent->GetComponentLevel();
+	SpawnInfo.ObjectFlags |= RF_Transient;
+	AAIController* NewController = GameStateComponent->GetWorld()->SpawnActor<AAIController>(BotControllerClass,
+		FVector::ZeroVector, FRotator::ZeroRotator, SpawnInfo);
+
+	if (NewController != nullptr)
+	{
+		check(GameMode);
+		if (NewController->PlayerState != nullptr)
+		{
+			const int32 RandomBotNumber = FMath::RandRange(10000, 99999);
+			const FString BotName = FString::Printf(TEXT("Enemy %d"), RandomBotNumber);
+			NewController->PlayerState->SetPlayerName(BotName);
+		}
+
+		GameMode->DispatchPostLogin(NewController);
+		GameMode->RestartPlayer(NewController);
+
+		if (NewController->GetPawn() != nullptr)
+		{
+			if (ULyraPawnExtensionComponent* PawnExtComponent = NewController->GetPawn()->FindComponentByClass<ULyraPawnExtensionComponent>())
+			{
+				PawnExtComponent->CheckDefaultInitialization();
+			}
+		}
+	}
 }
